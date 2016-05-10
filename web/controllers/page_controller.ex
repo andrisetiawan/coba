bd146cbdb54ed88a4ec15c7ed171e3cfaaa81afb
andri setiawan <andrisetiawan@me.com>
2016-05-10 22:10:06 +0700

    reply message

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -6,32 +6,34 @@ defmodule Infobola.PageController do
     render conn, "index.html"
   end
 
-  def webhook(conn, params) do
-    Logger.info(params)
+  def webhook(conn, %{ "message" => %{ "chat" => chat, "date" => date, "from" => from, "message_id" => message_id, "text" => text }}) do
+    # %{
+    #   "message" => %{
+    #     "chat" => %{
+    #       "first_name" => "Andri",
+    #       "id" => 82911682,
+    #       "last_name" => "Setiawan",
+    #       "type" => "private",
+    #       "username" => "andriset"
+    #     },
+    #     "date" => 1462851529,
+    #     "from" => %{
+    #       "first_name" => "Andri",
+    #       "id" => 82911682,
+    #       "last_name" => "Setiawan",
+    #       "username" => "andriset"
+    #     },
+    #     "message_id" => 9,
+    #     "text" => "adsfasf"
+    #   },
+    #   "update_id" => 949329732
+    # }
+
+    reply_message = "hi, " <> from["first_name"] <> " kamu nulis: " <> text
+    Yocingo.send_message(chat["id"], reply_message)
+    log = from["first_name"] <> ":" <> text
+    Logger.info(log)
     # Yocingo.send_message(id, Handler.parse(text, from))
     render conn, "index.html"
   end
-
-  # def parse(text, from) do
-  #
-  #   text = String.replace(text, "@InfobolaBot", "")
-  #         |> String.strip
-  #         |> String.downcase
-  #   answer = answer(text)
-  #
-  #   if answer do
-  #     answer
-  #   else
-  #     from <> ", " <> text # JUST A PONG MESSAGE
-  #   end
-  # end
-  #
-  # defp answer("hello") do
-  #   "hey!"
-  # end
-  #
-  # defp answer(_question) do
-  #   nil
-  # end
-
 end

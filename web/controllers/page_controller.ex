a2af71e56ed1af0b3053037ef999e10b8519249f
andri setiawan <andrisetiawan@me.com>
2016-05-10 22:55:48 +0700

    fix reply format

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -30,7 +30,11 @@ defmodule Infobola.PageController do
     # }
 
     reply_message = "hi, " <> from["first_name"] <> " kamu nulis: " <> text
-    Yocingo.send_message(chat["id"], reply_message)
+    Yocingo.send_message(chat["id"], reply_message, false, nil, %{ "force_reply" => false })
+    # Yocingo.send_message(82911682, "asfasdf ogitu",false,nil, %{ "force_reply" => false })
+    # Yocingo.send_message(82911682, "ya atau ngga?", false, nil, %{ "keyboard" => [ [%{ "text" => "yes"}, %{ "text" => "no"}]], "one_time_keyboard" => true, "resize_keyboard" => true })
+    # Yocingo.send_message(82911682, "ya atau ngga?", false, nil, %{ "keyboard" => [ [%{ "text" => "yes", "request_location" => true}, %{ "text" => "no"}]], "one_time_keyboard" => true, "resize_keyboard" => true })
+    # Yocingo.send_message(82911682, "ya atau ngga?", false, nil, %{ "hide_keyboard" => true })
     log = from["first_name"] <> ":" <> text
     Logger.info(log)
     # Yocingo.send_message(id, Handler.parse(text, from))
